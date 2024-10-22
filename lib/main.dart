01a003681466d43df7a25f6b2c38c92c957d0ec9
김동현 <71127040+DongHyeonKim-tech@users.noreply.github.com>
2024-10-22 13:17:06 +0900

    #3.6 Cards, #3.7 Icons and Transforms

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       home: Scaffold(
         // appBar: AppBar(
         //   title: Center(
@@ -20,16 +20,17 @@ class MyApp extends StatelessWidget {
         //   ),
         //   backgroundColor: Colors.blue,
         // ),
-        backgroundColor: Color(0xFF181818),
+        backgroundColor: const Color(0xFF181818),
         body: Padding(
-            // padding: const EdgeInsets.all(10), // 전체
-            // padding: EdgeInsets.only(right: 20), // 한 부분
-            // padding: EdgeInsets.symmetric(vertical: 10, horizontal: 20), // 수직, 수평
-            padding: EdgeInsets.symmetric(horizontal: 20),
-            child:
-                Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
-              SizedBox(height: 80),
-              Row(
+          // padding: const EdgeInsets.all(10), // 전체
+          // padding: EdgeInsets.only(right: 20), // 한 부분
+          // padding: EdgeInsets.symmetric(vertical: 10, horizontal: 20), // 수직, 수평
+          padding: const EdgeInsets.symmetric(horizontal: 20),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const SizedBox(height: 80),
+              const Row(
                 // row justify
                 mainAxisAlignment: MainAxisAlignment.end,
                 children: [
@@ -56,30 +57,30 @@ class MyApp extends StatelessWidget {
                   )
                 ],
               ),
-              SizedBox(
+              const SizedBox(
                 height: 30,
               ),
-              Text(
+              const Text(
                 'Total Balance',
                 style: TextStyle(
                   color: Colors.white30,
-                  fontSize: 22,
+                  fontSize: 20,
                 ),
               ),
-              SizedBox(
+              const SizedBox(
                 height: 10,
               ),
-              Text(
+              const Text(
                 '\$5 194 482',
                 style: TextStyle(
                   color: Colors.white,
-                  fontSize: 42,
+                  fontSize: 32,
                 ),
               ),
-              SizedBox(
+              const SizedBox(
                 height: 20,
               ),
-              Row(
+              const Row(
                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: [
                   // Container는 HTML의 div와 비슷
@@ -92,8 +93,97 @@ class MyApp extends StatelessWidget {
                       bgColor: Color(0xFF1F2123),
                       textColor: Colors.white),
                 ],
+              ),
+              const SizedBox(
+                height: 30,
+              ),
+              const Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                crossAxisAlignment: CrossAxisAlignment.end,
+                children: [
+                  Text('Wallets',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 30,
+                        fontWeight: FontWeight.w600,
+                      )),
+                  Text('View All',
+                      style: TextStyle(
+                        color: Colors.white30,
+                      )),
+                ],
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              Container(
+                // 영역을 벗어난 내부 요소 처리
+                clipBehavior: Clip.hardEdge,
+                decoration: BoxDecoration(
+                  color: const Color(0xFF1F2123),
+                  borderRadius: BorderRadius.circular(25),
+                ),
+                child: Padding(
+                  padding: const EdgeInsets.all(25),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      const Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            'Euro',
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 20,
+                              fontWeight: FontWeight.w600,
+                            ),
+                          ),
+                          SizedBox(
+                            height: 10,
+                          ),
+                          Row(
+                            children: [
+                              Text(
+                                '6 428',
+                                style: TextStyle(
+                                  color: Colors.white,
+                                  fontSize: 16,
+                                ),
+                              ),
+                              SizedBox(
+                                width: 5,
+                              ),
+                              Text(
+                                'EUR',
+                                style: TextStyle(
+                                  color: Colors.white30,
+                                  fontSize: 16,
+                                ),
+                              ),
+                            ],
+                          )
+                        ],
+                      ),
+                      // padding 등에 영향받지 않고 크기 조정
+                      Transform.scale(
+                          scale: 2,
+                          // padding 등에 영향받지 않고 위치 조정
+                          child: Transform.translate(
+                            offset: const Offset(-5, 12),
+                            child: const Icon(
+                              Icons.euro_rounded,
+                              color: Colors.white,
+                              size: 60,
+                            ),
+                          ))
+                    ],
+                  ),
+                ),
               )
-            ])),
+            ],
+          ),
+        ),
       ),
     );
   }
